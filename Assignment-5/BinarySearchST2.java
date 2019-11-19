2eca65e6a3451bd07fec3abce954e9ef200657c5
divyalakshmi1011 <divya.vallabhapurapu@gmail.com>
2019-11-19 17:07:42 +0530

    max

---
+++ Assignment-5/BinarySearchST2.java
@@ -0,0 +1,332 @@
+import java.util.NoSuchElementException;
+// import java.util.Queue;
+
+/**
+ * This is an API for Binart search Synbol Table.
+ * which consists of following methods
+ * void put (Key k, Value v) ----- inserts key and a value at appropriate position in the arrays.
+ * boolean contains(Key k) ----- return true if the given key is in the symboltable.
+ * Value get(Key k) ----- return value paired with Key.
+ * Key max() ----- return largest key
+ * Key floor(Key key) ---- return largest key less than or equal to key
+ * int rank(Key key) ---- return number of keys less than key
+ * void deleteMin() ---- delete smallest key
+ * Iterable keys() ---- return all keys, in sorted order
+ * @author Divya
+ */
+public class BinarySearchST2<Key extends Comparable<Key>, Value> {
+      /**
+       * keys is an arrays of all keys.
+       */
+       private Key[] keys;
+       /**
+        * vals is an array of values corresponding to keys.
+        */
+       private Value[] vals;
+       /**
+        * n is thenumber of keys in the keys array.
+        */
+       private int n;
+       /**
+        * size is the size of the array.
+        */
+       private int size;
+       /**
+        * capacity is the capabality of array to store elements.
+        */
+       int capacity = 5;
+    
+
+       /**
+       * Initializes an empty symbol table with the specified initial capacity.
+       */
+        public BinarySearchST2() {
+            keys = (Key[]) new Comparable[capacity];
+            vals = (Value[]) new Object[capacity];
+            size = 0;
+            n = 0;
+        }
+
+        // resize the underlying arrays
+        private void resize(int capacity) {
+            assert capacity >= n;
+            Key[]   tempk = (Key[])   new Comparable[capacity];
+            Value[] tempv = (Value[]) new Object[capacity];
+            for (int i = 0; i < n; i++) {
+                tempk[i] = keys[i];
+                tempv[i] = vals[i];
+            }
+            vals = tempv;
+            keys = tempk;
+        }
+        public int size() {
+            return n;
+        }
+
+        /**
+        * Returns true if this symbol table is empty.
+        *
+        * @return {@code true} if this symbol table is empty;
+        *         {@code false} otherwise
+        */
+        public boolean isEmpty() {
+          return n == 0;
+        }
+    
+    
+        /**
+     * Does this symbol table contain the given key?
+     *
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+    
+    
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     *
+     * @param  key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *         and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null"); 
+        if (isEmpty()) return null;
+        int i = rank(key); 
+        if (i < n && keys[i].compareTo(key) == 0) return vals[i];
+        return null;
+    } 
+     
+    
+    /**
+     * Returns the number of keys in this symbol table strictly less than {@code key}.
+     *
+     * @param  key the key
+     * @return the number of keys in the symbol table strictly less than {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public int rank(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to rank() is null"); 
+
+        int lo = 0, hi = n-1; 
+        while (lo <= hi) { 
+            int mid = lo + (hi - lo) / 2; 
+            int cmp = key.compareTo(keys[mid]);
+            if      (cmp < 0) hi = mid - 1; 
+            else if (cmp > 0) lo = mid + 1; 
+            else return mid; 
+        } 
+        return lo;
+    }
+   
+    
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the old 
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param  key the key
+     * @param  val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val)  {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null"); 
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        if(isEmpty()){
+            keys[n] = key;
+            vals[n] = val;
+            n++;
+            return;
+        }
+        if(keys[n-1].compareTo(key) == -1) {
+            // System.out.println("max");
+            keys[n] = key;
+            vals[n] = val;
+            n++;
+            return;
+        }
+        int i = rank(key);
+        // key is already in table
+        if (i < n && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+        // insert new key-value pair
+        if (n == keys.length) resize(2*keys.length);
+
+        for (int j = n; j > i; j--)  {
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+    } 
+    
+    
+    /**
+     * Removes the specified key and associated value from this symbol table
+     * (if the key is in the symbol table).
+     *
+     * @param  key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null"); 
+        if (isEmpty()) return;
+
+        // compute rank
+        int i = rank(key);
+
+        // key not in table
+        if (i == n || keys[i].compareTo(key) != 0) {
+            return;
+        }
+
+        for (int j = i; j < n-1; j++)  {
+            keys[j] = keys[j+1];
+            vals[j] = vals[j+1];
+        }
+
+        n--;
+        keys[n] = null;  // to avoid loitering
+        vals[n] = null;
+
+        // resize if 1/4 full
+        if (n > 0 && n == keys.length/4) resize(keys.length/2);
+    } 
+    
+    
+    /**
+     * Returns the largest key in this symbol table.
+     *
+     * @return the largest key in this symbol table
+     * @throws NoSuchElementException if this symbol table is empty
+     */
+    public Key max() {
+        if (isEmpty()) throw new NoSuchElementException("called max() with empty symbol table");
+        return keys[n-1];
+    }
+     
+    
+    /**
+     * Return the kth smallest key in this symbol table.
+     *
+     * @param  k the order statistic
+     * @return the {@code k}th smallest key in this symbol table
+     * @throws IllegalArgumentException unless {@code k} is between 0 and
+     *        <em>n</em>–1
+     */
+    public Key select(int k) {
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException("called select() with invalid argument: " + k);
+        }
+        return keys[k];
+    }
+
+    /**
+     * Returns the largest key in this symbol table less than or equal to {@code key}.
+     *
+     * @param  key the key
+     * @return the largest key in this symbol table less than or equal to {@code key}
+     * @throws NoSuchElementException if there is no such key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Key floor(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to floor() is null"); 
+        int i = rank(key);
+        if (i < n && key.compareTo(keys[i]) == 0) return keys[i];
+        if (i == 0) return null;
+        else return keys[i-1];
+    }
+
+    /**
+     * Returns the smallest key in this symbol table greater than or equal to {@code key}.
+     *
+     * @param  key the key
+     * @return the smallest key in this symbol table greater than or equal to {@code key}
+     * @throws NoSuchElementException if there is no such key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Key ceiling(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to ceiling() is null"); 
+        int i = rank(key);
+        if (i == n) return null; 
+        else return keys[i];
+    }
+    /**
+     * Removes the smallest key and associated value from this symbol table.
+     *
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow error");
+        delete(min());
+    }
+    /**
+     * Returns the smallest key in this symbol table.
+     *
+     * @return the smallest key in this symbol table
+     * @throws NoSuchElementException if this symbol table is empty
+     */
+    public Key min() {
+        if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
+        return keys[0];
+    }
+
+    /**
+     * Returns all keys in this symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in this symbol table
+     */
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    /**
+     * Returns all keys in this symbol table in the given range,
+     * as an {@code Iterable}.
+     *
+     * @param lo minimum endpoint
+     * @param hi maximum endpoint
+     * @return all keys in this symbol table between {@code lo} 
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+    public Iterable<Key> keys(Key lo, Key hi) {
+        if (lo == null) throw new IllegalArgumentException("first argument to keys() is null"); 
+        if (hi == null) throw new IllegalArgumentException("second argument to keys() is null"); 
+
+        Queue<Key> queue = new Queue<Key>(); 
+        if (lo.compareTo(hi) > 0) return queue;
+        for (int i = rank(lo); i < rank(hi); i++) 
+            queue.enqueue(keys[i]);
+        if (contains(hi)) queue.enqueue(keys[rank(hi)]);
+        return queue; 
+    }
+    public static void main(String[] args) {
+        BinarySearchST2<Integer, Integer> b = new BinarySearchST2<Integer, Integer>();
+        b.put(0,1);
+        b.put(2,3);
+        b.put(10,3);
+        b.put(1000,7);
+        b.put(2,9);
+    }
+}
+
+    
